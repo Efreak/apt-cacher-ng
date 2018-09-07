12b93f63adff35da1ee11d94faded92d1120ebba
Eduard Bloch <blade@debian.org>
2018-09-07 15:02:18 +0200

    Build tests for all interesting profiles

--- test/apptests.sh
+++ test/apptests.sh
@@ -8,3 +8,11 @@ bash -x test/cmdline/apt-cacher-ng.sh
 . test/include/start.sh $(mktemp -d) builddir/apt-cacher-ng
 (cd test/soap && bash -x soaptest.sh)
 finish_acng
+
+# build test with all profiles
+rm -rf tmp/buildtest-*
+for pro in Debug Release MinSizeRel ; do
+	mkdir -p tmp/buildtest-$pro
+	(cd tmp/buildtest-$pro && cmake ../.. -DCMAKE_BUILD_TYPE=$pro && make -j$(nproc))
+done
+
